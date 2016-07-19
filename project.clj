75bc20c68c703bcd3d8552ff4fe3fd16def82948
Brian Scaturro <scaturrob@gmail.com>
2016-07-18 21:39:02 -0400

    Bumps version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pradpi "0.1.0"
+(defproject pradpi "0.1.1"
   :description "A library to simplify working with the Amazon Product Advertising API"
   :url "https://github.com/brianium/pradpi"
   :license {:name "Eclipse Public License"
