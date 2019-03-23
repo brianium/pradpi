604e8ca13d8e84948b13aadbdb5eed89fe52a0b9
FossiFoo <fossifoo@gmail.com>
2019-03-23 06:21:14 +0100

    Fix wrong expectation in group test.

--- test/pradpi/group_test.clj
+++ test/pradpi/group_test.clj
@@ -6,7 +6,7 @@
   (testing "default :list-paths"
     (let [config (group/xml-config {})]
       (is (= [[:items :item]] (:list-paths config)))))
-  
+
   (testing "response group with offers"
     (let [config (group/xml-config {:ResponseGroup "Offers"})]
       (is (= [[:items :item]
@@ -15,4 +15,5 @@
   (testing "response group with offers and attributes"
     (let [config (group/xml-config {:ResponseGroup "Offers,ItemAttributes"})]
       (is (= [[:items :item]
+              [:items :item :item-attributes :upc-list :upc-list-element]
               [:items :item :offers :offer]] (:list-paths config))))))
